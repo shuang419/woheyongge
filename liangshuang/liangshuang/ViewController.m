2d6d575c7ab010e3ed2a83e7971b887a29c7d350
陈立勇 <chenlistyx@163.com>
2019-05-17 17:29:19 +0800

    5：29--新改动

--- liangshuang/liangshuang/ViewController.m
+++ liangshuang/liangshuang/ViewController.m
@@ -16,18 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //再加上冲突
-    
     // Do any additional setup after loading the view, typically from a nib.
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 100, 100)];
-    label.textColor = [UIColor whiteColor];
-    label.backgroundColor = [UIColor redColor];
-    label.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:label];
-    //小梁更改
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    
+    //测试1
+    //测试2
+    //测试3
 }
 
 
