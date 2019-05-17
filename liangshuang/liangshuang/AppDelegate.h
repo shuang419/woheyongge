e1ea596f6794fff949b4b79ff43e863329a4bc98
apple <apple@liangshuang.local>
2019-05-17 14:54:21 +0800

    feat:创建项目

---
+++ liangshuang/liangshuang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liangshuang
+//
+//  Created by apple on 2019/5/17.
+//  Copyright © 2019年 liangshuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ liangshuang/liangshuang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  liangshuang
+//
+//  Created by apple on 2019/5/17.
+//  Copyright © 2019年 liangshuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
