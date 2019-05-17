5529defe2bb2c2a5a1c93966c620d67456435eb9
陈立勇 <chenlistyx@163.com>
2019-05-17 16:00:06 +0800

    加入label

--- liangshuang/liangshuang/ViewController.m
+++ liangshuang/liangshuang/ViewController.m
@@ -17,8 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    NSLog(@"你好啊");
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 100, 100)];
+    label.textColor = [UIColor whiteColor];
+    label.backgroundColor = [UIColor redColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
 
 }
 
