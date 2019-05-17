7941855ea74f73939a58c739c7d6c7fdf0a97444
陈立勇 <chenlistyx@163.com>
2019-05-17 16:49:18 +0800

    label

--- liangshuang/liangshuang/ViewController.m
+++ liangshuang/liangshuang/ViewController.m
@@ -17,12 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(50, 100, 100, 100)];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
+    [btn addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 100, 100)];
     label.textColor = [UIColor whiteColor];
     label.backgroundColor = [UIColor redColor];
     label.textAlignment = NSTextAlignmentCenter;
     [self.view addSubview:label];
-
 }
 
 
